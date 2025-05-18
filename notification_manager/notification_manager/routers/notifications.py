03d4dffffea563aa4e4cb527cda7419d4d47dce6
Kar977 <karolmatuszak7@gmail.com>
2025-05-18 12:27:25 +0200

    [REFACTOR] used black

--- notification_manager/notification_manager/routers/notifications.py
+++ notification_manager/notification_manager/routers/notifications.py
@@ -1,12 +1,14 @@
 from fastapi import APIRouter
 from fastapi.responses import JSONResponse
 from notification_manager.schemas import SendEmailRequest
+import logging
+
+logging.basicConfig(format="%(levelname)s:%(message)s", level=logging.INFO)
 
 router = APIRouter(prefix="/notification")
 
 
 @router.post("/send-email")
 async def send_email(email_request: SendEmailRequest):
-    print("Sending email with details:", email_request, flush=True)
-
+    logging.INFO(f"Sending email with details: {email_request}")
     return JSONResponse(content={"status": "Email sent"}, status_code=200)
